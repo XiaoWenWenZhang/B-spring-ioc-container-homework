2f16af27b0cdac32e6bd09b453fb8b43b5333ce5
xiaowen <846703636@qq.com>
2020-11-08 21:14:03 +0800

    Injecting applicationContext

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 GreetingService 现在必须为 prototype scope，如何保证每次 GreetingController 的 greet() 方法被调用时都会
-创建新的 
+创建新的 greetingService bean？ 
+
+答：
 
 
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,10 +1,16 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
-@Service
-public class GreetingService {
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
 
+@Service
+@Scope(SCOPE_PROTOTYPE)
+public class GreetingService {
+    public GreetingService(){
+        System.out.println("create new  greetingService bean");
+    }
     String sayHi() {
         return "hello world";
     }
